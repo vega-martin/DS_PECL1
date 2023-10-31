6f353fffa6780e880281c851840c7764ab0e87d5
vega-martin <vega.martin@edu.uah.es>
2023-10-31 16:11:18 +0100

    top value in stack class modificated

--- DS_PECL1/Stack.cpp
+++ DS_PECL1/Stack.cpp
@@ -7,13 +7,13 @@ template <typename T> // Explained in header
 Stack::Stack(int capaity){
     this->capacity = capacity;
     element = new T[capacity];
-    top = -1; 
+    top = capacity + 1; 
 }
 
 // Function to insert elements in the Stack
 void Stack::insert(const T &value){ // access the value by reference and make it read only with "const"
-    if (top < capacity - 1){
-        top++;
+    if (top > 1){
+        top--;
         element[top] = value;
     } else {
         throw std::overflow_error("Stack is full!!!");
@@ -22,7 +22,7 @@ void Stack::insert(const T &value){ // access the value by reference and make it
 
 // Function to pop an element from the Stack (LIFO)
 T Stack::extract(){
-    if(top >= 0){
+    if(top <= capacity){
         return data[top--]; //devuelve data[top] y luego top--
     } else {
         throw std::underflow_error("Stack is empty!!!");
@@ -31,13 +31,13 @@ T Stack::extract(){
 
 // Function to determinte if the Stack is empty
 bool Stack::isEmpty(){
-    return top == -1;
+    (top >= capacity) ? true : false; // if top equal o greater than capacity retuns true else return false
 }
 
 bool Stack::isFull(){
-    return top == capacity - 1;
+    (top == 1) ? true : false; // if top equals to 1 retuns true (it is full) else return false
 }
 
 void Stack::makeNull(){
-    top = -1;
+    top = capacity + 1;
 }
